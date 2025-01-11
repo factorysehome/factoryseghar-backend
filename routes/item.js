16c6bc7ba0430f2781e98427bb94a7da9576a0f7
auriga <mohit.vijay@aurigait.com>
2025-01-11 17:45:31 +0530

    added item api

--- routes/item.js
+++ routes/item.js
@@ -39,12 +39,49 @@ router.post("/addItem", async (req, res) => {
 
 router.post("/getItems", async (req, res) => {
   const { category } = req.body;
-  if (category === "ALL") {
-    const response = await AddItemSchema.find({ category: "electronics" });
-    res.status(200).json({
-      status: "success",
-      message: "Item reterived successfully",
-      data: response,
+  try {
+    if (category === "ALL") {
+      const response = await AddItemSchema.find();
+      console.log(response);
+
+      const newResponse = response.map((item) => {
+        let base64Image = null;
+
+        // Convert binary image to Base64
+        if (item.image && item.image.data) {
+          base64Image = `data:image/png;base64,${item.image.data.toString(
+            "base64"
+          )}`;
+        }
+
+        // Return the required fields and the base64Image
+        return {
+          name: item.name,
+          variants: item.variants,
+          sku: item.sku,
+          description: item.description,
+          category: item.category,
+          base64Image, // Optional Base64 image
+        };
+      });
+
+      res.status(200).json({
+        status: "success",
+        message: "Items retrieved successfully",
+        data: newResponse,
+      });
+    } else {
+      const response = await AddItemSchema.find({ category: category });
+      res.status(200).json({
+        status: "success",
+        message: "Item reterived successfully",
+        data: response,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: "failed",
+      message: "Something went wront",
     });
   }
 });
