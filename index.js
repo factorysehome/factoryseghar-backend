0322632f822d7aff53d66f12eed86fcaea79aaa7
auriga <mohit.vijay@aurigait.com>
2025-01-13 11:26:58 +0530

    Fixed payload too large error

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const addItem = require("./routes/item");
 const address = require("./routes/address");
 const connectDB = require("./config/db");
 app.use(cors());
-app.use(express.json());
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
 //auth routes
 app.use("/auth", Auth);
