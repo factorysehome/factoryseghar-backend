67adfc421ef6d456b9ede946581a112c42661ba3
auriga <mohit.vijay@aurigait.com>
2025-01-15 11:45:40 +0530

    auth changes

--- models/Order.js
+++ models/Order.js
@@ -5,11 +5,30 @@ const orderSchema = new mongoose.Schema({
   customerName: { type: String, required: true },
   items: [
     {
-      productId: { type: String, required: true },
+      productName: { type: String, required: true },
       quantity: { type: Number, required: true },
       price: { type: Number, required: true },
     },
   ],
+  address: {
+    name: { type: String, required: true },
+    phone: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+      default: "",
+    },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
   totalAmount: { type: Number, required: true },
   razorpayOrderId: { type: String, required: true },
   razorpayPaymentId: { type: String },
--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const orderSchema = require("../models/Order");
 
 const router = express.Router();
+function generateOrderId() {
+  const timestamp = Date.now(); // Current timestamp in milliseconds
+  const random = Math.floor(Math.random() * 1000); // Random number between 0-999
+  return `ORD-${timestamp}-${random}`;
+}
 
 router.post("/placeOrder", async (req, res) => {
-  const { items, totalAmount, cashbackUser } = req.body;
+  const { items, totalAmount, mobile, address } = req.body;
 
   try {
+    const newOrder = new orderSchema({
+      orderId: generateOrderId(),
+      items,
+      totalAmount,
+      mobile,
+      address,
+    });
+    await newOrder.save();
   } catch (err) {
     res.status(500).json({
       message: "SOmething went wrong",
