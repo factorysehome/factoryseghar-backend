812fd4bd83f535929a0ea8b1b5c4d1b4cee8d848
auriga <mohit.vijay@aurigait.com>
2025-01-16 14:40:36 +0530

    added placeorder , paymentverification, and orderhistory api

--- models/Order.js
+++ models/Order.js
@@ -2,16 +2,38 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   orderId: { type: String, required: true, unique: true },
-  // customerName: { type: String, required: true },
+  customerName: { type: String, required: true },
+  mobile: { type: String, required: true },
   items: [
     {
-      productId: { type: String, required: true },
+      productName: { type: String, required: true },
+      productDetail: { type: {} },
       quantity: { type: Number, required: true },
       price: { type: Number, required: true },
     },
   ],
-
   totalAmount: { type: Number, required: true },
+  cashbackUsed: { type: Number },
+  address: {
+    name: { type: String, required: true },
+    phone: {
+      type: String,
+      required: true,
+    },
+    addressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2: {
+      type: String,
+      default: "",
+    },
+    landMark: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    pinCode: { type: String, required: true },
+    country: { type: String, required: true },
+  },
   razorpayOrderId: { type: String, required: true },
   razorpayPaymentId: { type: String },
   paymentStatus: {
